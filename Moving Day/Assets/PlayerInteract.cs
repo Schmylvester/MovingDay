023fcd94907c935b0d1af10aa41151f5f57b0325
brynfduwe <bryn2.findlay-dykes@live.uwe.ac.uk>
2019-01-26 13:22:38 +0000

    Player can now pick up things, and also drop things

---
+++ Moving Day/Assets/PlayerInteract.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public GameObject grabbedObj;
+    [SerializeField] private Transform grabObjectPos;
+
+    private float dropOverLapDelay;
+    bool grabbed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    //delay so drop input isnt dected instantly (could be couroutine wait???)
+        if (dropOverLapDelay > 0.25f)
+	    {
+	        InputManager iM = FindObjectOfType<InputManager>();
+	        if (iM.buttonUp(XboxButton.B, GetComponent<PlayerMovement>().GetPlayerID()))
+	        {
+	            DropObject();
+	        }
+	    }
+	    else
+	    {
+	        if (grabbed)
+	        {
+	            dropOverLapDelay += Time.deltaTime;
+            }
+	    }
+	}
+
+
+    void DropObject()
+    {
+        if (grabbedObj != null)
+        {
+            grabbedObj.transform.parent = null;
+            grabbedObj.GetComponent<Rigidbody>().AddForce(GetComponent<PlayerMovement>().GetPlayerForceDirection(), ForceMode.Impulse);
+            grabbedObj.GetComponent<Rigidbody>().useGravity = true;
+
+
+            grabbedObj = null;
+            grabbed = false;
+            dropOverLapDelay = 0;
+        }
+    }
+
+
+    void GrabObject(GameObject _grab_gobj)
+    {
+        if (grabbedObj == null)
+        {
+            if (_grab_gobj.GetComponent<InteractObject>() != null)
+            {
+                grabbedObj = _grab_gobj;
+                grabbedObj.transform.parent = transform;
+                grabbedObj.transform.position = grabObjectPos.position;
+                grabbedObj.GetComponent<Rigidbody>().useGravity = false;
+
+                grabbed = true;
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider _col)
+    {
+        InputManager iM = FindObjectOfType<InputManager>();
+        if (iM.buttonDown(XboxButton.B, GetComponent<PlayerMovement>().GetPlayerID()))
+        {
+            GrabObject(_col.gameObject);
+        }
+    }
+}
